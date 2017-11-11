3226fb1c2a630fe8d44b604553d7bea20f1f47fc
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-11 04:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function lowerCaseDrivers(drivers) {
     return driver.toLowerCase();
   });
 }
+
+function nameToAttributes(names) {
+  return names.map(function(name) {
+    return Object.assign({firstname: name.split(' ')[0] , lastname: name.split(' ')[1]});
+  })
+}
